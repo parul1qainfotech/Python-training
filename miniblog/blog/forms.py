712205ae83b6f9e512275a64a37e0ded90f988c6
parul1 <parul1@qainfotech.com>
2021-03-10 20:23:02 +0530

    miniblog

---
+++ miniblog/blog/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import Blog
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField
+from django.contrib.auth.models import User
+from django.utils.translation import gettext, gettext_lazy as _
+
+
+class SignUpForm(UserCreationForm):
+    password1=forms.CharField(label="Password",widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2=forms.CharField(label="Confirm Password (again)",widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    class Meta:
+        model=User
+        fields= ['username','first_name','last_name','email']
+        labels= {'first_name':'First Name','last_name':'Last Name','email':'Email'}
+        widgets={'username':forms.TextInput(attrs={'class':'form-control'}),
+                 'first_name':forms.TextInput(attrs={'class':'form-control'}),
+                 'last_name':forms.TextInput(attrs={'class':'form-control'}),
+                 'email':forms.TextInput(attrs={'class':'form-control'}),
+                 }
+        
+class LoginForm(AuthenticationForm):
+    
+    username = UsernameField(widget=forms.TextInput(attrs={'class':'form-control','autofocus': True}))
+    password = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password','class':'form-control'}),
+    )
+    
+class AddBlog(forms.ModelForm):
+    class Meta:
+        model=Blog
+        fields=['title','description']
+        widgets={'title':forms.TextInput(attrs={'class':'form-control'}),
+                 'description':forms.Textarea(attrs={'class':'form-control'})
+                 }
